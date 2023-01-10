244f202940e04f42d788224b7551a02315b6c09f
karolina <karolinalodzinska98@gmail.com>
2023-01-10 13:14:06 +0100

    The player cannot move into walls

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
@@ -1,6 +1,7 @@
 package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.CellType;
 
 public class Player extends Actor {
     public Player(Cell cell) {
@@ -10,4 +11,14 @@ public class Player extends Actor {
     public String getTileName() {
         return "player";
     }
+
+    public void move(int dx, int dy) {
+        Cell nextCell = cell.getNeighbor(dx, dy);
+        if (nextCell.getType() != CellType.WALL) {
+            cell.setActor(null);
+            nextCell.setActor(this);
+            cell = nextCell;
+        }
+    }
+
 }
